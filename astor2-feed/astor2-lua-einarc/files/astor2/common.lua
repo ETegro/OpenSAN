0145fdb1282a5ccf7009c0ec846d9477331fe3ea
Sergey Matveev <stargrave@stargrave.org>
2011-03-13 19:16:43 +0300

    Some Lua bindings for Einarc
    * Untested OpenWRT's package Makefile for installing this Lua library
      directly from package directory
    * Common (astor2.common) library that currently includes single system()
      function that is capable of intercepting stdout, stderr and return
      code under all circumstances. Lua's default os.system() can not handle
      std* and io.popen() can not check return code. However even with
      failure return code we need to parse it's stderr sometimes. For
      example to deal with NotImplemented raised Ruby exception
    * astor2.einarc.{logical|physical}.list() functions returning table with
      corresponding correctly parsed data from Einarc. IDs are table's keys
    * Heavy usage of Lua's logging library capabilities
    Signed-off-by: Sergey Matveev <stargrave@stargrave.org>

---
+++ astor2-feed/astor2-lua-einarc/files/astor2/common.lua
@@ -0,0 +1,71 @@
+--[[
+  aStor2 -- storage area network configurable via Web-interface
+  Copyright (C) 2009-2011 ETegro Technologies, PLC
+                          Sergey Matveev <stargrave@stargrave.org>
+  
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as
+  published by the Free Software Foundation, either version 3 of the
+  License, or (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+  
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+]]
+
+module( "astor2.common", package.seeall )
+
+require "logging.console"
+logger = logging.console()
+
+local SHELL_PATH = "/bin/sh"
+
+function system( cmdline )
+	assert( cmdline )
+	logger:debug( "common:system() called with cmdline \"" .. cmdline .. "\"" )
+	stdout_path = os.tmpname()
+	stderr_path = os.tmpname()
+	script_path = os.tmpname()
+	assert( stdout_path and stderr_path and script_path )
+
+	-- Script to be executed itself
+	script_fd = io.open( script_path, "w" )
+	script_fd:write( cmdline .. "\n" )
+	script_fd:close()
+
+	result = {}
+
+	-- Execute command and retreive return code
+	logger:debug( "common:system() executing script " .. script_path )
+	result.return_code = os.execute( SHELL_PATH .. " "
+	                                 .. script_path
+	                                 .. " >" .. stdout_path
+	                                 .. " 2>" .. stderr_path )
+	os.remove( script_path )
+
+	-- Read it's stdout
+	result.stdout = {}
+	logger:debug( "common:system() parsing stdout " .. stdout_path )
+	stdout_fd = io.open( stdout_path, "r" )
+	for line in stdout_fd:lines() do
+		result.stdout[ #result.stdout + 1 ] = line
+	end
+	stdout_fd:close()
+	os.remove( stdout_path )
+
+	-- Read it's stderr
+	result.stderr = {}
+	logger:debug( "common:system() parsing stderr " .. stderr_path )
+	stderr_fd = io.open( stderr_path, "r" )
+	for line in stderr_fd:lines() do
+		result.stderr[ #result.stderr + 1 ] = line
+	end
+	stderr_fd:close()
+	os.remove( stderr_path )
+
+	return result
+end
---
+++ astor2-feed/astor2-lua-einarc/files/astor2/einarc.lua
@@ -0,0 +1,96 @@
+--[[
+  aStor2 -- storage area network configurable via Web-interface
+  Copyright (C) 2009-2011 ETegro Technologies, PLC
+                          Sergey Matveev <stargrave@stargrave.org>
+  
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as
+  published by the Free Software Foundation, either version 3 of the
+  License, or (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+  
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+]]
+
+module( "astor2.einarc", package.seeall )
+require( "astor2.common" )
+local EINARC_CMD = "einarc -t software -a 0 "
+local logger = astor2.common.logger
+
+function run( args )
+	assert( args )
+	result = astor2.common.system( EINARC_CMD .. args )
+	if result.return_code ~= 0 then return nil end
+	return result.stdout
+end
+
+logical = {}
+
+logical.list = function()
+	-- TODO
+	-- #  RAID level  Physical drives  Capacity  Device  State
+	logger:debug( "einarc:logical.list() called" )
+	output = run( "logical list" )
+	if not output or #output == 0 then
+		logger:warn( "einarc:logical.list() no logical disks" )
+		return {}
+	end
+	logicals = {}
+	for i, line in ipairs( output ) do
+		id = string.match( line, "^([0-9]+)" )
+		assert( id )
+		logicals[ id ] = {
+			level = string.match( line, "^[0-9]+\t([0-9]+)\t[0-9:,]\t.*\t.*\t.*$" ) or "",
+			drives = string.match( line, "^[0-9]+\t[0-9]+\t([0-9:,])\t.*\t.*\t.*$" ) or "",
+			capacity = tonumber( string.match( line, "^[0-9]+\t[0-9]+\t[0-9:,]\t([0-9\.]+)\t.*\t.*$" ) ) or 0,
+			device = string.match( line, "^[0-9]+\t[0-9]+\t[0-9:,]\t.*\t(.*)\t.*$" ) or "",
+			state = string.match( line, "^[0-9]+\t[0-9]+\t[0-9:,]\t.*\t.*\t(.*)$" ) or ""
+		}
+		logger:info( "einarc:logicals.list() ID " ..
+		             string.format( "%s [ %d, %s, %f, %q, %s ]", id,
+		                            logicals[ id ].level,
+		                            logicals[ id ].drives,
+		                            logicals[ id ].capacity,
+		                            logicals[ id ].device,
+		                            logicals[ id ].state ) )
+	end
+	return logicals
+end
+
+physical = {}
+
+physical.list = function()
+	-- ID   Model       Revision  Serial        Size     State
+	-- 1:0  ST980310AS            5ST05LK2  76319.09 MB  free
+	logger:debug( "einarc:physical.list() called" )
+	output = run( "physical list" )
+	if not output or #output == 0 then
+		logger:warn( "einarc:physical.list() no physical disks" )
+		return {}
+	end
+	physicals = {}
+	for i, line in ipairs( output ) do
+		id = string.match( line, "^([0-9:]+)" )
+		assert( id )
+		physicals[ id ] = {
+			model = string.match( line, "^[0-9:]+\t(.*)\t.*\t.*\t.*\t.*$" ) or "",
+			revision = string.match( line, "^[0-9:]+\t.*\t(.*)\t.*\t.*\t.*$" ) or "",
+			serial = string.match( line, "^[0-9:]+\t.*\t.*\t(.*)\t.*\t.*$" ) or "",
+			size = tonumber( string.match( line, "^[0-9:]+\t.*\t.*\t.*\t([0-9\.]+)\t.*$" ) ) or 0,
+			state = string.match( line, "^[0-9:]+\t.*\t.*\t.*\t.*\t(.*)$" ) or ""
+		}
+		logger:info( "einarc:physical.list() ID " ..
+		             string.format( "%s [ %q, %q, %q, %f, %s ]", id,
+		                            physicals[ id ].model,
+		                            physicals[ id ].revision,
+		                            physicals[ id ].serial,
+		                            physicals[ id ].size,
+		                            physicals[ id ].state ) )
+	end
+	return physicals
+end
