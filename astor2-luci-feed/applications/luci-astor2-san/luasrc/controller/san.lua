48f7b00ba0103dd9e635d2dc99480bbbaeb095f5
Vladimir Petukhov <vladimir.petukhov@etegro.com>
2011-03-16 14:22:18 +0300

    Moved physical list to separate template Signed-off-by: Vladimir Petukhov <vladimir.petukhov@etegro.com>

--- astor2-luci-feed/applications/luci-astor2-san/luasrc/controller/san.lua
+++ astor2-luci-feed/applications/luci-astor2-san/luasrc/controller/san.lua
@@ -1,10 +1,10 @@
-module("luci.controller.san", package.seeall)
+module( "luci.controller.san", package.seeall )
 
 function index()
-	entry({"san"}, call("physical_list"), "San", 10)
+	entry( { "san" }, call( "einarc_lists" ), "San", 10 ) --transkate
 end
 
-local phys_list_result =
+local physical_list_result =
 	{ ["0:1"] = { model = "WDC WD5000BPVT-0",
 		revision = "01.0",
 		serial = "000003HYJJK",
@@ -26,6 +26,30 @@ local phys_list_result =
 		size = 15328.00,
 		state = "free" } }
 
-function physical_list()
-	luci.template.render("einarc/einarc", {phys_list_result=phys_list_result})
+local logical_list_result =
+	{ ["0:1"] = { model = "WDC WD5000BPVT-0",
+		revision = "01.0",
+		serial = "000003HYJJK",
+		size = 476940.02,
+	state = "free" },
+	["0:2"] = { model = "Transcend 8GB",
+		revision = "8.07",
+		serial = "",
+		size = 7664.00,
+		state = "free" },
+	["1:0"] = { model = "Kingston 16GB",
+		revision = "9.65",
+		serial = "rev5",
+		size = 15328.00,
+		state = "free" },
+	["1:1"] = { model = "Samsung 16GB",
+		revision = "8.5",
+		serial = "rev1",
+		size = 15328.00,
+		state = "free" } }
+
+function einarc_lists()
+	luci.template.render( "san",
+		{ physical_list = physical_list_result,
+		  logical_list = logical_list_result } )
 end
