7e8c801e855fb8844aa5b507ca1b0efa0a420cbe
Sergey Matveev <stargrave@stargrave.org>
2011-03-16 17:59:57 +0300

    Use real einarc.physical.list() call Signed-off-by: Sergey Matveev <stargrave@stargrave.org>

--- astor2-luci-feed/applications/luci-astor2-san/luasrc/controller/san.lua
+++ astor2-luci-feed/applications/luci-astor2-san/luasrc/controller/san.lua
@@ -1,30 +1,10 @@
 module( "luci.controller.san", package.seeall )
 
+einarc = require "astor2.einarc"
+
 function index()
 	entry( { "san" }, call( "einarc_lists" ), "San", 10 ) --transkate
 end
-
-local physical_list_result =
-	{ ["0:1"] = { model = "WDC WD5000BPVT-0",
-		revision = "01.0",
-		serial = "000003HYJJK",
-		size = 476940.02,
-	state = "free" },
-	["0:2"] = { model = "Transcend 8GB",
-		revision = "8.07",
-		serial = "",
-		size = 7664.00,
-		state = "free" },
-	["1:0"] = { model = "Kingston 16GB",
-		revision = "9.65",
-		serial = "rev5",
-		size = 15328.00,
-		state = "free" },
-	["1:1"] = { model = "Samsung 16GB",
-		revision = "8.5",
-		serial = "rev1",
-		size = 15328.00,
-		state = "free" } }
 
 local logical_list_result =
 	{ ["0:1"] = { model = "WDC WD5000BPVT-0",
@@ -50,6 +30,6 @@ local logical_list_result =
 
 function einarc_lists()
 	luci.template.render( "san",
-		{ physical_list = physical_list_result,
+		{ physical_list = einarc.physical.list(),
 		  logical_list = logical_list_result } )
 end
