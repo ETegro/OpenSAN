ef1f79d56db158881fbd4177206c79865fbf3aaa
Sergey Matveev <stargrave@stargrave.org>
2011-03-17 18:26:06 +0300

    i18n of RAID error levels

--- astor2-luci-feed/applications/luci-astor2-san/luasrc/controller/san.lua
+++ astor2-luci-feed/applications/luci-astor2-san/luasrc/controller/san.lua
@@ -23,10 +23,10 @@ module( "luci.controller.san", package.seeall )
 common = require( "astor2.common" )
 einarc = require( "astor2.einarc" )
 
-function index()
-	require( "luci.i18n" ).loadc( "astor2_san")
-	local i18n = luci.i18n.translate
+require( "luci.i18n" ).loadc( "astor2_san")
 
+function index()
+	local i18n = luci.i18n.translate
 	local e = entry( { "san" }, call( "einarc_lists" ), i18n("SAN"), 10 )
 	e.i18n = "astor2_san"
 
@@ -49,16 +49,24 @@ local function is_valid_raid_level( raid_level )
 end
 
 local function is_valid_raid_configuration( raid_level, drives )
+	local i18n = luci.i18n.translate
 	local VALIDATORS = {
-		["linear"] = function( drives ) return #drives == 1 end,
-		["passthrough"] = function( drives ) return #drives == 1 end,
-		["0"] = function( drives ) return #drives >= 2 end,
-		["1"] = function( drives ) return #drives >= 2 and common.is_odd( #drives ) end,
-		["5"] = function( drives ) return #drives >= 3 end,
-		["6"] = function( drives ) return #drives >= 3 and common.is_odd( #drives ) end,
-		["10"] = function( drives ) return #drives >= 4 and common.is_odd( #drives ) end
+		["linear"] = { validator = function( drives ) return #drives == 1 end,
+		               message = i18n("Linear level requires exactly single drive") },
+		["passthrough"] = { validator = function( drives ) return #drives == 1 end,
+		                    message = i18n("Passthrough level requries exactly single drive") },
+		["0"] = { validator = function( drives ) return #drives >= 2 end,
+		          message = i18n("0 level requires two or more drives") },
+		["1"] = { validator = function( drives ) return #drives >= 2 and common.is_odd( #drives ) end,
+		          message = i18n("1 level requries odd number two or more drives") },
+		["5"] = { validator = function( drives ) return #drives >= 3 end,
+		          message = i18n("5 level requires three or more drives") },
+		["6"] = { validator = function( drives ) return #drives >= 3 and common.is_odd( #drives ) end,
+		          message = i18n("6 level requires odd number of four or more drives") },
+		["10"] = { validator = function( drives ) return #drives >= 4 and common.is_odd( #drives ) end,
+		           message = i18n("10 level requires odd number or four or more drives") }
 	}
-	return VALIDATORS[ raid_level ]( drives )
+	return VALIDATORS[ raid_level ].validator( drives ), VALIDATORS[ raid_level ].message
 end
 
 local function index_with_error( message_error )
@@ -71,28 +79,26 @@ function logical_add()
 	local drives = luci.http.formvalue( "drives" )
 	local raid_level = luci.http.formvalue( "raid_level" )
 	local message_error = nil
+	local i18n = luci.i18n.translate
 
 	if common.is_string( drives ) then
 		drives = { drives }
 	end
 
 	if not drives then
-		message_error = "drives not selected"
+		message_error = i18n("Drives not selected")
 	else
 		if not is_valid_raid_level( raid_level ) then
-			message_error = "Incorrect RAID level"
-		elseif not is_valid_raid_configuration ( raid_level, drives ) then
-			message_error = "Incorrect RAID configuration"
+			message_error = i18n("Incorrect RAID level")
 		else
-			einarc.logical.add( raid_level, drives )
+			local is_valid, message = is_valid_raid_level( raid_level )
+			if is_valid then
+				einarc.logical.add( raid_level, drives )
+			else
+				message_error = message
+			end
 		end
 	end
 
 	index_with_error( message_error )
 end
-
---	else type( drives ) == table then
---		luci.http.write( "Drives: " .. table.concat( drives ) .. " end" )
---[[		luci.http.redirect( luci.dispatcher.build_url( "san" ) .. "/" .. luci.http.build_querystring( { message="Selected: " .. table.concat( drives ) } ) )
-	elseif type( drives ) == string then
-		luci.http.redirect( luci.dispatcher.build_url( "san" ) .. "/" .. luci.http.build_querystring( { message="please select minimum 2 drives" } ) )--]]
