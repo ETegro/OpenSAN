9ebe5d49988853e58dc6384187cdaf535bc3fb3e
Sergey Matveev <stargrave@stargrave.org>
2011-04-14 12:35:26 +0400

    Try to use our own OpenWRT's commited version Signed-off-by: Sergey Matveev <stargrave@stargrave.org>

--- build/build.sh
+++ build/build.sh
@@ -20,7 +20,7 @@
 
 WORK_DIR=`echo $0 | sed "s/\/build.sh$//"`
 pushd "$WORK_DIR"; WORK_DIR=`pwd`; popd
-TARGET_DIR="$WORK_DIR"/openwrt/trunk
+TARGET_DIR="$WORK_DIR"/../openwrt
 OUTPUT_DIR="$WORK_DIR"/output/`date "+%Y-%m-%dT%H:%M"`-$BRANCH
 DL_DIR="$TARGET_DIR"/dl
 BIN_DIR="$TARGET_DIR"/bin/x86
@@ -33,60 +33,10 @@ mmake()
 
 . "$WORK_DIR"/build.conf
 
-checkout_openwrt()
-{
-	$SVN -r$OPENWRT_TRUNK_REVISION checkout $OPENWRT_TRUNK_URL "$TARGET_DIR"
-}
-
-check_openwrt_existence()
-{
-	[ -d "$TARGET_DIR" ] || mkdir -p "$TARGET_DIR"
-	[ -d "$TARGET_DIR"/.svn ] || checkout_openwrt
-	pushd "$TARGET_DIR"
-	$SVN update -r$OPENWRT_TRUNK_REVISION || checkout_openwrt
-	popd
-}
-
-update_feeds_configuration()
-{
-	local feeds_conf=feeds.conf.default
-
-	pushd "$TARGET_DIR"
-	cp /dev/null "$feeds_conf"
-	echo "$FEEDS" | while read feed; do
-		echo "Feed: $feed"
-		feed_type=`echo "$feed" | awk '{print $1}'`
-		feed_name=`echo "$feed" | awk '{print $2}'`
-		feed_path=`echo "$feed" | awk '{print $3}'`
-		revision=`echo "$feed" | awk '{print $4}'`
-		[ "$feed_type" != "src-link" ] || feed_path="$WORK_DIR"/"$feed_path"
-		echo "$feed_type $feed_name $feed_path" >> "$feeds_conf"
-		./scripts/feeds update $feed_name
-		if [ "$revision" = "" ]; then
-			true
-		else
-			case $feed_type in
-			"src-svn")
-				pushd feeds/$feed_name
-				$SVN update -r$revision
-				popd
-				;;
-			*)
-				echo "Unsupported feed type for retreiving specified version"
-				exit 1
-				;;
-			esac
-			./scripts/feeds update -i $feed_name
-		fi
-		./scripts/feeds install -a -p $feed_name
-	done
-	popd
-}
-
 update_openwrt_config()
 {
 	rm -f "$TARGET_DIR"/.config
-	ln -s "$WORK_DIR"/.config "$TARGET_DIR"/.config
+	git checkout HEAD "$TARGET_DIR"/.config
 }
 
 remove_dl_directory()
@@ -128,11 +78,9 @@ copy_bins()
 	cp -a "$BIN_DIR"/* "$OUTPUT_DIR"/
 }
 
-check_openwrt_existence
 update_openwrt_config
 remove_dl_directory
 perform_cleaning
-update_feeds_configuration
 update_openwrt_config
 create_dl_directory
 create_output_directory
