7dfe8f97e5ae08ef0d0521a04704be47c9391668
Sergey Matveev <stargrave@stargrave.org>
2011-04-15 19:16:05 +0400

    Fixed snapshot's success checking Signed-off-by: Sergey Matveev <stargrave@stargrave.org>

--- feeds/astor2/astor2-lua-lvm/files/astor2/lvm.lua
+++ feeds/astor2/astor2-lua-lvm/files/astor2/lvm.lua
@@ -267,7 +267,7 @@ function M.LogicalVolume:snapshot( size )
 				      self.device )
 	local succeeded = false
 	for _, line in ipairs( output.stdout ) do
-		if string.match( line, "Logical volume \"%w+\" created" ) then
+		if string.match( line, "Logical volume \".+\" created" ) then
 			succeeded = true
 		end
 	end
