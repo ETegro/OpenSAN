971f7e9beb0a19273e134192d452ccae597f1267
Sergey Matveev <stargrave@stargrave.org>
2011-04-26 12:47:56 +0400

    Draft version of configuration dumping function Signed-off-by: Sergey Matveev <stargrave@stargrave.org>

--- feeds/astor2/astor2-lua-scst/files/astor2/scst.lua
+++ feeds/astor2/astor2-lua-scst/files/astor2/scst.lua
@@ -125,4 +125,71 @@ function M.AccessPattern:unbind()
 	self:save()
 end
 
+function M.AccessPattern:is_binded()
+	if self.filename then
+		return true
+	else
+		return false
+	end
+end
+
+------------------------------------------------------------------------
+-- Configuration
+------------------------------------------------------------------------
+M.Configuration = {}
+local Configuration_mt = common.Class( M.Configuration )
+
+function M.Configuration.dump()
+	-- Collect only enabled and binded patterns
+	local access_patterns_enabled = {}
+	for _, access_pattern in ipairs( M.AccessPattern.list() ) do
+		if access_pattern.enabled and access_pattern.is_binded() then
+			access_patterns_enabled[ #access_patterns_enabled + 1 ] = access_pattern
+		end
+	end
+
+	local configuration = ""
+
+	-- Create HANDLERs
+	local blockios = {}
+	local blockio_counter = 1
+	configuration = configuration .. "HANDLER vdisk_blockio {\n"
+	for _, access_pattern in ipairs( access_patterns_enabled  ) do
+		blockios[ access_pattern.filename ] = blockio_counter
+		configuration = configuration .. "\tDEVICE blockio" .. blockio_counter .. " {\n"
+		configuration = configuration .. "\t\tfilename " .. access_pattern.filename .. "\n"
+		configuration = configuration .. "\t}\n"
+		blockio_counter = blockio_counter + 1
+	end
+	configuration = configuration .. "}\n"
+
+	-- Create TARGET_DRIVERs
+	local access_patterns_target_drivers = common.unique_keys( "targetdriver", access_patterns_enabled )
+	for target_driver, access_patterns_indexes in pairs( access_patterns_target_drivers ) do
+		if target_driver == "iscsi" then
+			configuration = configuration .. "TARGET_DRIVER iscsi {\n"
+			configuration = configuration .. "\tTARGET iqn.2006-10.net.vlnb:tgt {\n" -- TODO
+			for _, access_patterns_index in ipairs( access_patterns_indexes ) do
+				access_pattern = access_patterns_enabled[ access_patterns_index ]
+				local read_only = nil
+				if access_pattern.readonly then
+					read_only = "1"
+				else
+					read_only = "0"
+				end
+				configuration = configuration ..
+				                "\t\tLUN " ..
+				                tostring( access_pattern.lun ) ..
+						" blockio" ..
+						tostring( blockios[ access_pattern.filename ] ) ..
+						" {\n" ..
+						"\t\t\tread_only " .. read_only .. "\n" ..
+						"\t\t}\n"
+			end
+			configuration = configuration .. "\t}\n" -- TODO
+			configuration = configuration .. "}\n"
+		end
+	end
+end
+
 return M
