1affff3dcaaf8faf65f6469490b0e607076586e5
Vladimir Petukhov <vladimir.petukhov@etegro.com>
2011-04-27 16:53:02 +0400

    Added a sketch of the function function scst_access_pattern_bind()

--- feeds/luci/applications/luci-astor2-san/luasrc/controller/san.lua
+++ feeds/luci/applications/luci-astor2-san/luasrc/controller/san.lua
@@ -495,6 +495,30 @@ local function scst_access_pattern_delete( inputs )
 	index_with_error( message_error )
 end
 
+local function scst_access_pattern_bind( inputs )
+	local i18n = luci.i18n.translate
+	local message_error = nil
+
+	local access_pattern_section_name = nil
+	for k, v in pairs( inputs ) do
+		if not access_pattern_section_name then
+			-- san.submit_access_pattern_bind-cfg022eb2
+			access_pattern_section_name = string.match( k, "^submit_access_pattern_bind.(%w+)$" )
+		end
+	end
+	assert( access_pattern_section_name )
+
+	logical_volume_device = nil
+
+	local return_code, result = pcall( scst.AccessPattern.bind,
+		                           { section_name = access_pattern_section_name },
+		                           filename = logical_volume_device )
+	if not return_code then
+		message_error = i18n("Failed to bind access pattern") .. ": " .. result
+	end
+	index_with_error( message_error )
+end
+
 ------------------------------------------------------------------------
 -- Different common functions
 ------------------------------------------------------------------------
@@ -530,7 +554,8 @@ function perform()
 		logical_volume_snapshot_add = function() lvm_logical_volume_snapshot_add( inputs ) end,
 		logical_volume_snapshot_resize = function() lvm_logical_volume_snapshot_resize( inputs ) end,
 		access_pattern_create = function() scst_access_pattern_new( inputs ) end,
-		access_pattern_delete = function() scst_access_pattern_delete( inputs ) end
+		access_pattern_delete = function() scst_access_pattern_delete( inputs ) end,
+		access_pattern_bind = function() scst_access_pattern_bind( inputs ) end
 	}
 
 	for _, submit in ipairs( common.keys( inputs ) ) do
