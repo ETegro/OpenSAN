ce5cfb8d067ab081c8b1c8b275d3d1b8b4a24dae
Vladimir Petukhov <vladimir.petukhov@etegro.com>
2011-05-06 15:00:58 +0400

    Edited code for better reading

--- feeds/luci/applications/luci-astor2-san/luasrc/controller/matrix.lua
+++ feeds/luci/applications/luci-astor2-san/luasrc/controller/matrix.lua
@@ -218,7 +218,7 @@ function M.filter_highlight_snapshots( matrix, colors_array )
 				color_number = color_number + 1
 			end
 
-			if line.logical_volume.is_snapshot() == false then
+			if not line.logical_volume.is_snapshot() then
 				lines[ current_line ].logical_volume.highlight.background_color = color
 
 				if #line.logical_volume.snapshots ~= 0 then
