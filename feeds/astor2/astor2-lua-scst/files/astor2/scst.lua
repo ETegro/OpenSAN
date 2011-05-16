4456e1f3a86fbb3f0bbe749feb61d601f70da842
Sergey Matveev <stargrave@stargrave.org>
2011-05-16 13:53:19 +0400

    Force deletion operation without prompting while applying configuration Signed-off-by: Sergey Matveev <stargrave@stargrave.org>

--- feeds/astor2/astor2-lua-scst/files/astor2/scst.lua
+++ feeds/astor2/astor2-lua-scst/files/astor2/scst.lua
@@ -300,6 +300,8 @@ function M.Daemon.apply()
 	M.Configuration.write( configuration,
 	                       M.Configuration.SCSTADMIN_CONFIG_PATH )
 	common.system_succeed( M.Daemon.SCSTADMIN_PATH ..
+	                       "-force " ..
+	                       "-noprompt " ..
 	                       " -config " ..
 	                       M.Configuration.SCSTADMIN_CONFIG_PATH )
 end
