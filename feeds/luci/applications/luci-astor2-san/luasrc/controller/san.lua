3d195f871555ec82cad8b680079656e675e92f0b
Sergey Matveev <stargrave@stargrave.org>
2011-05-17 13:16:47 +0400

    Moving from SHA1 to SHA256 because of performance issues Signed-off-by: Sergey Matveev <stargrave@stargrave.org>

--- feeds/luci/applications/luci-astor2-san/luasrc/controller/san.lua
+++ feeds/luci/applications/luci-astor2-san/luasrc/controller/san.lua
@@ -27,16 +27,23 @@ matrix = require( "luci.controller.matrix" )
 scst = require( "astor2.scst" )
 
 mime = require( "mime" )
-require( "sha1" )
 
--- Calculate SHA1 regular expression
+------------------------------------------------------------------------
+-- Hash related functions
+------------------------------------------------------------------------
+require( "sha2" )
+
+-- Calculate SHA2-256 regular expression
 hashre = ""
-for i = 1, 160 / 8 * 2 do hashre = hashre .. "." end
+for i = 1, 256 / 8 * 2 do hashre = hashre .. "." end
+
+local function hash( data )
+	return sha2.sha256hex( data )
 
 local function find_by_hash( hash, objs )
 	local found = nil
 	for _, obj in ipairs( objs ) do
-		if sha1( tostring( obj ) ) == hash then
+		if hash( tostring( obj ) ) == hash then
 			found = obj
 		end
 	end
@@ -225,7 +232,7 @@ local function einarc_logical_hotspare_add( inputs, data )
 		return index_with_error( i18n("Logical disk is not selected") )
 	end
 
-	if tonumber( inputs[ "logical_minimal_size-" .. physical_id_hash .. "-" .. sha1( tostring( logical_id ) ) ] ) <
+	if tonumber( inputs[ "logical_minimal_size-" .. physical_id_hash .. "-" .. hash( tostring( logical_id ) ) ] ) <
 	   tonumber( inputs[ "physical_size-" .. physical_id_hash ] ) then
 		message_error = i18n("Newly added hotspare disk is bigger than needed")
 	end
