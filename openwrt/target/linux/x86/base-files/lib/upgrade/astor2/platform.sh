32c99076da8a5b7915c5ad0e3b0ef8092e86566e
Sergey Matveev <stargrave@stargrave.org>
2011-05-25 12:44:43 +0400

    Configuration tarballs are compressed Signed-off-by: Sergey Matveev <stargrave@stargrave.org>

--- openwrt/target/linux/x86/base-files/lib/upgrade/astor2/platform.sh
+++ openwrt/target/linux/x86/base-files/lib/upgrade/astor2/platform.sh
@@ -189,7 +189,7 @@ platform_copy_config()
 	local mountpoint=`mktemp -d`
 	mkdir -p $mountpoint
 	mount $FUTURE_ROOT $mountpoint
-	tar -x -f "$CONF_TAR" -C $mountpoint
+	tar -xz -f "$CONF_TAR" -C $mountpoint
 	umount $mountpoint
 	rmdir $mountpoint
 	sync
