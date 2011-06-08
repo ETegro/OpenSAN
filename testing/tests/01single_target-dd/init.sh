c8b55fee8c48f5b54c16ed57dfe4741fc2c0d24d
Sergey Matveev <stargrave@stargrave.org>
2011-06-08 12:08:59 +0400

    Copyright added for remote-testing framework Signed-off-by: Sergey Matveev <stargrave@stargrave.org>

--- testing/tests/01single_target-dd/init.sh
+++ testing/tests/01single_target-dd/init.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
+# aStor2 -- storage area network configurable via Web-interface
+# Copyright (C) 2009-2011 ETegro Technologies, PLC
+#                         Sergey Matveev <stargrave@stargrave.org>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 iqns_stop_all
 run_clearing
