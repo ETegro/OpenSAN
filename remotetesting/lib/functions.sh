cc848136f60105d421d63a462c88c4d3933abd9c
Sergey Matveev <stargrave@stargrave.org>
2011-06-14 22:36:00 +0400

    Check for Lua script's return code Signed-off-by: Sergey Matveev <stargrave@stargrave.org>

--- remotetesting/lib/functions.sh
+++ remotetesting/lib/functions.sh
@@ -81,10 +81,13 @@ run_lua()
 	CMD_SCP "$luasrc" /tmp/"$luasrc_name"
 	CMD_SCP $WORK_DIR/lib/luaunit.lua /usr/lib/lua/luaunit.lua
 	local run_result=`mktemp`
-	CMD_SSH "PATH=/bin:/sbin:/usr/bin:/usr/sbin lua /tmp/$luasrc_name 2>&1" |
-		tee $run_result
+	CMD_SSH "PATH=/bin:/sbin:/usr/bin:/usr/sbin
+		lua /tmp/$luasrc_name 2>&1 ||
+		touch /tmp/${luasrc_name}.failed" |
+			tee $run_result
 	grep -q "^Failed" $run_result && is_failed=1 || true
-	CMD_SSH rm -f /tmp/"$luasrc_name"
+	CMD_SSH test -e "/tmp/${luasrc_name}.failed" && is_failed=1 || true
+	CMD_SSH rm -f /tmp/"$luasrc_name" "/tmp/${luasrc_name}.failed"
 	rm -f $luasrc $run_result
 	[ "$is_failed" = "" ] || return 1
 }
