3e5af7c70960fe4b22aff396eba485d70999de31
Sergey Matveev <stargrave@stargrave.org>
2011-10-11 12:38:01 +0400

    [DOC] Remote wiki updating script Signed-off-by: Sergey Matveev <stargrave@stargrave.org>

---
+++ wiki/update_remote.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+REMOTE_USER=root
+REMOTE_HOST=opensan-trac.etegro.local
+REMOTE_PATH=/srv
+
+scp -r ../wiki $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH/wiki
+ssh $REMOTE_USER@$REMOTE_HOST "cd $REMOTE_PATH/wiki ; ./insert_all.sh"
