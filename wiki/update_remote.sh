180c6163d66c78b45ad9ef0fba1f6dbd660a07c0
Sergey Matveev <stargrave@stargrave.org>
2011-10-12 22:26:48 +0400

    [DOC] Corrected remote wiki uploading script Signed-off-by: Sergey Matveev <stargrave@stargrave.org>

--- wiki/update_remote.sh
+++ wiki/update_remote.sh
@@ -4,5 +4,6 @@ REMOTE_USER=root
 REMOTE_HOST=opensan-trac.etegro.local
 REMOTE_PATH=/srv
 
-scp -r ../wiki $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH/wiki
+ssh $REMOTE_USER@$REMOTE_HOST mkdir -p $REMOTE_PATH/wiki
+scp -r ../wiki/* $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH/wiki/
 ssh $REMOTE_USER@$REMOTE_HOST "cd $REMOTE_PATH/wiki ; ./insert_all.sh"
