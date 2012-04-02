f86d0a438bebbbfbdadad82b6987b89d681c27a1
Sergey Matveev <stargrave@stargrave.org>
2012-04-02 17:11:35 +0400

    [FIX] DM even handler module is required Signed-off-by: Sergey Matveev <stargrave@stargrave.org>

--- openwrt/package/kernel/modules/block.mk
+++ openwrt/package/kernel/modules/block.mk
@@ -183,9 +183,9 @@ define KernelPackage/dm
   # to stop the config from asking the question.
   # MIRROR is M because I've needed it for pvmove.
   KCONFIG:= \
-	CONFIG_BLK_DEV_MD=n \
+	CONFIG_BLK_DEV_MD \
 	CONFIG_DM_DEBUG=n \
-	CONFIG_DM_UEVENT=n \
+	CONFIG_DM_UEVENT \
 	CONFIG_DM_DELAY=n \
 	CONFIG_DM_MULTIPATH \
 	CONFIG_DM_ZERO=n \
