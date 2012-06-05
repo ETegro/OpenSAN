8e8d2ceda861ed4914d2fad615b8fe20d4ee247c
Sergey Matveev <stargrave@stargrave.org>
2012-06-05 12:15:41 +0400

    [DOC] All releases notes on single page Signed-off-by: Sergey Matveev <stargrave@stargrave.org>

--- wiki/news_relann.sh
+++ wiki/news_relann.sh
@@ -3,14 +3,6 @@
 WHAT=$1
 CONTENTS=$2
 
-curdate=`git show --date=iso8601 HEAD | sed -n "s/-//g; s/://g; s/^Date  *\(.*\) \(.*\).. +....$/\1T\2/p"`
-
-cat $CONTENTS > Release$WHAT.wiki
-cat >> Release$WHAT.wiki <<__EOF__
-
-=== Download ===
-You can download this release following [/download/V$WHAT/ this link].
-Always cryptographically [wiki:Publickey verify images]!
-__EOF__
-
-echo "* [wiki:Release$WHAT $WHAT]" >> Releases.wiki
+echo >> Releases.wiki
+echo "= $1 =" >> Releases.wiki
+cat $CONTENTS >> Releases.wiki
