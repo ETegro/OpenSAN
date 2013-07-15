b63d5e5176249c2a3dd408e88de6c80f5b7780bf
Difrex(Denis Zheleztsov) <dz@etegro.com>
2013-07-15 11:33:50 +0400

    [Alpha] Added web server for a test

---
+++ script/web_server.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+# This web server needs ONLY for a test!
+# Perfomance is shit but it's working :)
+
+use strict;
+use warnings;
+use utf8;
+
+use HTTP::Server::Brick;
+use HTTP::Status;
+    
+my $server = HTTP::Server::Brick->new( port => 5000 );
+    
+$server->mount( '/' => {
+	path => "/path/to/you/html/files/",
+});
+     
+# start accepting requests (won't return unless/until process
+# receives a HUP signal)
+$server->start;
