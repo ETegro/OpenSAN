78d0fe2efc03b1fb6780d268ab8f85376345cbeb
Difrex(Denis Zheleztsov) <dz@etegro.com>
2013-07-18 11:53:26 +0400

    Starts work on tables supporting

---
+++ script/SAN/Tables.pm
@@ -0,0 +1,15 @@
+package SAN::Tables;
+
+sub new() {
+    my $class = shift;
+    my $self = {};
+    bless $self, $class;
+    return $self;	
+}
+
+sub parse_tables() {
+	my ($class, $self) = shift;
+	
+}
+
+1;
